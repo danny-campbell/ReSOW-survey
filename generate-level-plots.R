29d6d77b667836eced8234c58ef872d41ecf0daf
danny-campbell <dycampbell@gmail.com>
2023-05-11 11:26:43 +0100

    naming of doughnut plots changed and added new functions and plots for status-quo levels

--- generate-level-plots.R
+++ generate-level-plots.R
@@ -26,28 +26,71 @@ doughnut <- function(prop.1, prop.2, col.1, col.2){
   text(0, -0.225, paste0("+", prop.2, "%"), cex = 2, col = grey(0.7), font = 2)
 }
 
+# function to generate the status-quo doughnut plot
+# agruments:
+# prop.1: status-quo proportion (as a %)
+# col.1: colour to represent status-quo proportion
+doughnut.sq <- function(prop.1, col.1){
+  require(circlize)
+  par(mar = c(0, 0, 0, 0), bg = rgb(1, 1, 1, 0))
+  plot(c(-0.96, 0.96), c(-0.96, 0.96), type = "n", axes = FALSE, ann = FALSE, 
+       asp = 1)
+  draw.sector(0, 360, rou1 = 1, rou2 = , clock.wise = TRUE, 
+              col = rgb(1, 1, 1, 0.85), lwd = 5, border = rgb(1, 1, 1, 0.85))
+  draw.sector(0, 360, rou1 = 1, rou2 = 0.55, clock.wise = TRUE, col = grey(0.9),
+              lwd = 5, border = "white")
+  draw.sector(90, 90 - (prop.1 * 3.6), rou1 = 1, rou2 = 0.55, clock.wise = TRUE,
+              col = col.1, lwd = 5, border = "white")
+  text(0, 0.15, paste0(prop.1, "%"), cex = 3, col = grey(0.55), 
+       font = 2)
+}
+
+
+
 # function to save the plot to a svg file 
-# note: stored in folder 'img' and takes the nae from the levels
+# note: stored in folder 'img' and takes the name from the levels
 doughnut.svg <- function(prop.1, prop.2, col.1, col.2){
-  svg(paste0("img/level-", prop.1, "-", prop.2, ".svg"), width = 2, height = 2,
-      bg = "transparent")
+  require(stringr)
+  svg(paste0("img/level-", str_pad(prop.1, 3, pad = "0"), "-",
+             str_pad(prop.2, 3, pad = "0"), ".svg"), 
+      width = 2, height = 2, bg = "transparent")
   doughnut(prop.1, prop.2, col.1, col.2)
   dev.off()   
 }
 
 # function to save the plot to a png file 
-# note: stored in folder 'img' and takes the nae from the levels
+# note: stored in folder 'img' and takes the name from the levels
 doughnut.png <- function(prop.1, prop.2, col.1, col.2){
-  svg(paste0("img/level-", prop.1, "-", prop.2, ".png"), width = 2, height = 2,
-      bg = "transparent")
+  svg(paste0("img/level-", str_pad(prop.1, 3, pad = "0"), "-",
+             str_pad(prop.2, 3, pad = "0"), ".png"), 
+      width = 2, height = 2, bg = "transparent")
   doughnut(prop.1, prop.2, col.1, col.2)
+  dev.off()   
+}
+
+# function to save the status-quo plot to a svg file 
+# note: stored in folder 'img' and takes the name from the levels
+doughnut.sq.svg <- function(prop.1, col.1){
+  require(stringr)
+  svg(paste0("img/level-", str_pad(prop.1, 3, pad = "0"), ".svg"), 
+      width = 2, height = 2, bg = "transparent")
+  doughnut.sq(prop.1, col.1)
+  dev.off()   
+}
+
+# function to save the status-quo  plot to a png file 
+# note: stored in folder 'img' and takes the name from the levels
+doughnut.sq.png <- function(prop.1, col.1){
+  svg(paste0("img/level-", str_pad(prop.1, 3, pad = "0"), ".png"), 
+      width = 2, height = 2, bg = "transparent")
+  doughnut.sq(prop.1, col.1)
   dev.off()   
 }
 
 
 # colours for status-quo and management levels
-col.1 <- "#DB4325" 
-col.2 <- "#006164"
+col.1 <- "#f23427" 
+col.2 <- "#f23427"
 
 # set the range for status-quo and the maximum after management
 min.sq <- 0 # as a %
@@ -78,5 +121,11 @@ proportion_combns <- proportion_combns[proportion_combns$sum <= max.after.policy
 for(i in 1:nrow(proportion_combns)){
   doughnut.svg(proportion_combns[i, 1], proportion_combns[i, 2], col.1, col.2)
   #  doughnut.png(proportion_combns[i, 1], proportion_combns[i, 2], col.1, col.2)
+}
+
+# plot and save the status-quo doughnut plots 
+for(i in seq_along(sq.levs)){
+  doughnut.sq.svg(sq.levs[i], col.1)
+  # doughnut.sq.png(sq.levs[i], col.1)
 }
 
